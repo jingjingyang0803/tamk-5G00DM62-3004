0c9d546bf3f0a021c67610cf70092cf6394f3054
Jingjing Yang <jingjing.yang@tuni.fi>
2024-02-08 10:37:05 +0200

    012/Print.java:add solution

---
+++ basic/012/Print.java
@@ -0,0 +1,37 @@
+import java.time.LocalDate;
+
+/**
+ * @author      Jingjing Yang <jingjing.yang@tuni.fi>
+ * @version     2024.0208 (last modified)
+ * @since       17.0 (miminimum Java version)
+ */
+class Print {
+
+	/**
+	 * Exercise: 1.2
+	 *    1.2 Program print
+	 *
+	 *         Write a program to display: date, your first name. last name,
+	 *         and height (in cm) to the screen. Store each information to a
+	 *         separate variable before displaying.
+	 *
+	 *             The date is YYYY-MM-DD
+	 *             Hello, I'm John Doe
+	 *             I'm 185 cm tall
+	 *
+	 * @param  args  Command line arguments. Not used.
+	 * @return No return value (void method).
+	 */
+	public static void main(String[] args) {
+		LocalDate date = LocalDate.now();
+		String firstName = "Jingjing"; // my first name
+		String lastName = "Yang"; // my last name
+		int height = 163;// my height in cm
+
+		System.out.println("The date is " + date);
+		System.out.println("Hello, I'm " + firstName + " " + lastName);
+		System.out.println("I'm " + height + " cm tall");
+	}
+}
+
+// End of file
