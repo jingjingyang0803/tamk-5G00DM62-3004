70276fbea3add00242d2170e55f1a2c10a2f8c1f
Jingjing Yang <jingjing.yang@tuni.fi>
2024-02-11 20:21:02 +0200

    Update format

--- basic/011/Hello.java
+++ basic/011/Hello.java
@@ -1,36 +1,37 @@
 /**
- * @author 	Jingjing Yang <jingjing.yang@tuni.fi>
- * @version     2024.0208 (last modified)
- * @since   	17.0 (miminimum Java version)
+ * @author Jingjing Yang <jingjing.yang@tuni.fi>
+ * @version 2024.0211 (last modified)
+ * @since 17.0 (miminimum Java version)
  */
 class Hello {
 
 	/**
+	 * Displays the user's name in a box created by *.
+	 *
 	 * Exercise: 1.1
-	 * 1.1 Program hello
-	 * <p>
-	 * Write a program to display your name in a box
-	 * <p>
-	 * ************************
-	 * * Hello, I am John Doe *
-	 * ************************
+	 * 		1.1 Program hello
+	 *
+	 * 		Write a program to display your name in a box
+	 *
+	 * 			************************
+	 * 			* Hello, I am John Doe *
+	 * 			************************
 	 *
 	 * @param args Command line arguments. Not used.
-  	 * @return No return value (void method).
 	 */
 	public static void main(String[] args) {
 		String name = "Jingjing Yang";
-		String content = "Hello, I am " + name; // The content to be displayed in the box
+		// The content to be displayed in the box
+		String content = "Hello, I am " + name;
 		char symbol = '*'; // The symbol used to create the box
 		box(content, symbol);
 	}
 
 	/**
-	 * This method prints a string content within a box made of a specified symbol.
+	 * Prints a string content within a box made of a specified symbol.
 	 *
 	 * @param content The string to be printed inside the box.
 	 * @param symbol  The character used to create the box around the content.
-	 * @return No return value (void method).
 	 */
 	public static void box(String content, char symbol) {
 		int length = content.length();
@@ -41,11 +42,12 @@ class Hello {
 	}
 
 	/**
-	 * This method prints a row of symbols, used as part of the box in the box method.
+	 * Prints a row of symbols, used as part of the box
+	 * in the box method.
 	 *
-	 * @param length The length of the content around which the box is to be created.
-	 * @param symbol The character used to create the row in the box.
-	 * @return No return value (void method).
+	 * @param length The length of the content around which
+	 *               the box is to be created.
+	 * @param symbol The character used to create the row in the box
 	 */
 	public static void row(int length, char symbol) {
 		for (int i = 0; i < length + 4; i++) {
--- basic/012/Print.java
+++ basic/012/Print.java
@@ -1,13 +1,15 @@
 import java.time.LocalDate;
 
 /**
- * @author 		Jingjing Yang <jingjing.yang@tuni.fi>
- * @version     2024.0208 (last modified)
- * @since   	17.0 (miminimum Java version)
+ * @author Jingjing Yang <jingjing.yang@tuni.fi>
+ * @version 2024.0211 (last modified)
+ * @since 17.0 (miminimum Java version)
  */
 class Print {
 
 	/**
+	 * Displays the user's first name. last name, and height (cm) to the screen.
+	 *
 	 * Exercise: 1.2
 	 *    1.2 Program print
 	 *
@@ -20,7 +22,6 @@ class Print {
 	 *             I'm 185 cm tall
 	 *
 	 * @param  args  Command line arguments. Not used.
-	 * @return No return value (void method).
 	 */
 	public static void main(String[] args) {
 		LocalDate date = LocalDate.now();
@@ -32,15 +33,16 @@ class Print {
 	}
 
 	/**
-	 * This method prints personal details including the current date, full name, and height.
+	 * Prints personal details including the current date,
+	 * full name, and height.
 	 *
 	 * @param date      The current date.
 	 * @param firstName The first name.
 	 * @param lastName  The last name.
 	 * @param height    The height in centimeters.
-	 * @return No return value (void method).
 	 */
-	public static void printPersonalDetails(LocalDate date, String firstName, String lastName, int height) {
+	public static void printPersonalDetails(LocalDate date, String firstName,
+											String lastName, int height) {
 		System.out.println("The date is " + date);
 		System.out.println("Hello, I'm " + firstName + " " + lastName);
 		System.out.println("I'm " + height + " cm tall");
