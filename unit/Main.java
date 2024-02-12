6a371024df586bbfcb3c0bfe1494f72ba05339b7
Jingjing Yang <jingjing.yang@tuni.fi>
2024-02-12 02:38:56 +0200

    Document Main.java

--- unit/Main.java
+++ unit/Main.java
@@ -1,4 +1,14 @@
+/**
+ * @author Jingjing Yang <jingjing.yang@tuni.fi>
+ * @version 2024.0211 (last modified)
+ * @since 17.0 (miminimum Java version)
+ */
 class Main {
+	/**
+	 * Serves as the entry point of the application.
+	 *
+	 * @param args Command line arguments passed to the application.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
