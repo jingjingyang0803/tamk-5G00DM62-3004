2c79bd6b450621d8e2730f01ecfb08de8f1b95ec
Jingjing Yang <jingjing.yang@tuni.fi>
2024-02-19 14:58:03 +0200

    Add required files

---
+++ project/Main.java
@@ -0,0 +1,16 @@
+/**
+ * @author Jingjing Yang <jingjing.yang@tuni.fi>
+ * @version 2024.0211 (last modified)
+ * @since 17.0 (miminimum Java version)
+ */
+public class Main {
+
+    /**
+     * Serves as the entry point of the application.
+     *
+     * @param args Command line arguments passed to the application.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
